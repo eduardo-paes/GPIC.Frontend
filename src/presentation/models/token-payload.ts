660c4070ec870c647be7a9f7ce2cbb9a5af954d1
PSThiago <thiago.paes.silva@hotmail.com.br>
2023-10-22 19:30:24 -0300

    feat: add field to receive professor or student id.

--- src/presentation/models/token-payload.ts
+++ src/presentation/models/token-payload.ts
@@ -4,6 +4,7 @@ export interface TokenPayload {
 	"http://schemas.microsoft.com/ws/2008/06/identity/claims/role": string;
 	"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/name": string;
 	"http://schemas.xmlsoap.org/ws/2005/05/identity/claims/sid": string;
+	"http://schemas.xmlsoap.org/ws/2009/09/identity/claims/actor": string;
 	iss: string;
 	jti: string;
 }
